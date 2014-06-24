13931e0c146fddceb0bf2e7d0e67e1dd243605a5
hung-phan <colorvisavn@gmail.com>
2014-06-24 11:16:57 +0700

    add component

---
+++ component/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var util = require('util');
+var yeoman = require('yeoman-generator');
+
+var ComponentGenerator = module.exports = function ComponentGenerator(args, options, config) {
+  // By calling `NamedBase` here, we get the argument to the subgenerator call
+  // as `this.name`.
+  yeoman.generators.NamedBase.apply(this, arguments);
+
+  console.log('You called the component subgenerator with the argument ' + this.name + '.');
+};
+
+util.inherits(ComponentGenerator, yeoman.generators.NamedBase);
+
+ComponentGenerator.prototype.files = function files() {
+  this.template('component.js.msx.coffee', 'app/assets/javascripts/' + this.name + '/' + this.name + '.js.msx.coffee');
+};
