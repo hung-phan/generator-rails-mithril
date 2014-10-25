22d3222cbb06fd7d0cbea203d050ea07e31cbfc9
hung-phan <colorvisavn@gmail.com>
2014-10-25 21:57:33 +0700

    working on gemfile

--- app/index.js
+++ app/index.js
@@ -218,6 +218,10 @@ var RailsMithrilGenerator = yeoman.generators.Base.extend({
     }
   },
 
+  rspecRails: function() {
+    shell.exec("rails generate rspec:install");
+  },
+
   defaultStylesheet: function() {
     console.log(magenta('Copy default.css.scss file'));
     this.template('app/default.css.scss', 'app/assets/stylesheets/default.css.scss');
